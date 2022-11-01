797c60dd35098bdb767095d10b0444216d37218f
Mara Guluzian <mara15.guluzian@gmail.com>
2022-11-01 18:19:08 -0400

    making keyboard emit noises

---
+++ src/main.ts
@@ -0,0 +1,24 @@
+import { sortUserPlugins } from 'vite'
+import './style.css'
+//keydown event is fired when a key is pressed
+const sounds = document.querySelectorAll('audio')
+const keys = document.querySelectorAll('.keys div')
+
+window.addEventListener('keydown', (event) => {
+  //the keycode
+  let code = event.keyCode.toString()
+
+  //Playing the audio if the keyCode is equal to the audio's ID
+  sounds.forEach((sound) => {
+    if (code === sound.id) {
+      sound.play()
+      sound.currentTime = 0
+    }
+  })
+  keys.forEach((key) => {
+    if (code === key.id) {
+      key.classList.add('playing')
+      setTimeout(() => key.classList.remove('playing'), 100)
+    }
+  })
+})
